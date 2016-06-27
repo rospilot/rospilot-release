e4439923e8478a311855a97bbb5c1632d099909a
Christopher Berner <christopherberner@gmail.com>
2016-06-26 19:54:04 -0700

    Rebase from 'release/indigo/rospilot'

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['rospilot'],
+    packages=['rospilot', 'rospilot.assets', 'vlc_server'],
     package_dir={'': 'src'},
+    package_data={'rospilot.assets': ['*.*', '*/*.*', '*/*/*.*']},
     requires=['std_msgs', 'rospy']
 )
 
